57ac3e03752daf0a71d9e1d33621249935f23bbb
ricardoteles <ricardo.oliveira.teles@gmail.com>
2016-05-24 00:52:16 -0300

    modo CBC funcionando

--- src/Arquivos.java
+++ src/Arquivos.java
@@ -5,20 +5,28 @@ import java.nio.file.StandardOpenOption;
 import java.io.IOException;
 
 public class Arquivos {
-  
+	static String VI = "11111111111111111111111111111111111111111111111111111111111111111111111111111111"+
+				"111111111111111111111111111111111111111111111111";
+	
     public static void CBC() throws IOException{
         byte[] fileBytes = readFile(Ep.arqEntrada);
         Path filePath = Paths.get(Ep.arqSaida);
         K128 k = new K128();
         String X = "";
-                
+        String Y = "";
+        
         for (int i = 0; i < fileBytes.length; i++) {
         	X += String.format("%8s", Integer.toBinaryString(fileBytes[i] & 0xFF)).replace(' ', '0');
 
         	if (i % 16 == 15){
-            	System.out.println(X.length());
-            	if(X.length() == 128) {
-            		writeFile(k.k128(X), filePath);
+        		if(X.length() == 128) {						//TODO: verificar se < 128 bits 
+	        		if(i == 15){
+	        			Y = k.k128(Operacoes.xor128(X, VI));
+	        		}
+	        		else{
+	        			Y = k.k128(Operacoes.xor128(X, Y));
+	        		}
+            		writeFile(Y, filePath);
             	}
             	X = "";
             }            
@@ -30,18 +38,28 @@ public class Arquivos {
         Path filePath = Paths.get(Ep.arqSaida);
         K128 k = new K128();
         String X = "";
-                
+        String Y = "";
+        String Yant = "";
+        
         for (int i = 0; i < fileBytes.length; i++) {
-        	X += String.format("%8s", Integer.toBinaryString(fileBytes[i] & 0xFF)).replace(' ', '0');
+        	Y += String.format("%8s", Integer.toBinaryString(fileBytes[i] & 0xFF)).replace(' ', '0');
 
         	if (i % 16 == 15){
-            	if(X.length() == 128) {					// TODO: tamanho do bloco < 128
-            		writeFile(k.k128Inv(X), filePath);
+        		if(Y.length() == 128) {
+	        		if(i == 15){
+	        			X = Operacoes.xor128(k.k128Inv(Y), VI);
+	        		}
+	        		else{
+	        			X = Operacoes.xor128(k.k128Inv(Y), Yant);
+	        		}
+            		writeFile(X, filePath);
             	}
-            	X = "";
+        		Yant = Y;
+            	Y = "";
             }            
         }        
     }
+
     
 	public static void writeFile(String b, Path filePath) throws IOException{
         byte[] fileBytes = new byte[16];
